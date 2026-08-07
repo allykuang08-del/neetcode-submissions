6ffca1b6de2cedb0cbda7b1211b2d8c7aad21f7e
allykuang08-del <allykuang08@gmail.com>
2026-08-07 18:42:57 -0500

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        import string
+
+        ## lowercase, gets rid of spaces, gets rid of punctuation
+        str1 = s.lower()
+        str2 = str1.replace(" ", "")
+        str3 = str2.translate(str.maketrans("", "", string.punctuation))
+
+        if str3 == str3[::-1]:
+            return True
+        else:
+            return False
